8d7f7c0078cc3b39871a72e657210f4d64a30967
helenNo1 <1460451166@qq.com>
2024-01-16 23:00:04 +0800

    httpsproxy fail

--- openssl/cs/s.cpp
+++ openssl/cs/s.cpp
@@ -10,12 +10,14 @@
 #define SERVER_PORT 12345
 #define BUFFER_SIZE 1024
 
-void handle_error() {
+void handle_error()
+{
     perror("Error");
     exit(EXIT_FAILURE);
 }
 
-SSL_CTX* init_server_context() {
+SSL_CTX *init_server_context()
+{
     SSL_CTX *ctx;
 
     SSL_library_init();
@@ -23,13 +25,15 @@ SSL_CTX* init_server_context() {
     SSL_load_error_strings();
 
     ctx = SSL_CTX_new(TLS_server_method());
-    if (ctx == NULL) {
+    if (ctx == NULL)
+    {
         ERR_print_errors_fp(stderr);
         handle_error();
     }
 
     if (SSL_CTX_use_certificate_file(ctx, "server.crt", SSL_FILETYPE_PEM) <= 0 ||
-        SSL_CTX_use_PrivateKey_file(ctx, "server.key", SSL_FILETYPE_PEM) <= 0) {
+        SSL_CTX_use_PrivateKey_file(ctx, "server.key", SSL_FILETYPE_PEM) <= 0)
+    {
         ERR_print_errors_fp(stderr);
         handle_error();
     }
@@ -37,7 +41,8 @@ SSL_CTX* init_server_context() {
     return ctx;
 }
 
-void start_server(SSL_CTX *ctx) {
+void start_server(SSL_CTX *ctx)
+{
     int server_fd, client_fd;
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_len = sizeof(client_addr);
@@ -45,7 +50,8 @@ void start_server(SSL_CTX *ctx) {
     SSL *ssl;
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
+    if (server_fd == -1)
+    {
         handle_error();
     }
 
@@ -54,19 +60,23 @@ void start_server(SSL_CTX *ctx) {
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(SERVER_PORT);
 
-    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
         handle_error();
     }
 
-    if (listen(server_fd, 10) == -1) {
+    if (listen(server_fd, 10) == -1)
+    {
         handle_error();
     }
 
     printf("Server listening on port %d...\n", SERVER_PORT);
 
-    while (1) {
-        client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &addr_len);
-        if (client_fd == -1) {
+    while (1)
+    {
+        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
+        if (client_fd == -1)
+        {
             handle_error();
         }
 
@@ -75,17 +85,21 @@ void start_server(SSL_CTX *ctx) {
         ssl = SSL_new(ctx);
         SSL_set_fd(ssl, client_fd);
 
-        if (SSL_accept(ssl) <= 0) {
+        if (SSL_accept(ssl) <= 0)
+        {
             ERR_print_errors_fp(stderr);
-        } else {
+        }
+        else
+        {
             char buffer[BUFFER_SIZE];
             memset(buffer, 0, sizeof(buffer));
 
             int len = SSL_read(ssl, buffer, sizeof(buffer) - 1);
-            if (len > 0) {
+            if (len > 0)
+            {
                 printf("Received message: %s\n", buffer);
 
-                const char* response = "Hello from server!";
+                const char *response = "Hello from server!";
                 SSL_write(ssl, response, strlen(response));
             }
         }
@@ -97,7 +111,8 @@ void start_server(SSL_CTX *ctx) {
     close(server_fd);
 }
 
-int main() {
+int main()
+{
     SSL_CTX *ctx = init_server_context();
     start_server(ctx);
 
